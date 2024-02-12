13a69c4f34efce56d1910a77c6f83eb347e5f5f9
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-13 00:09:16 +0100

    triangleArea Calculation card done!

--- js/traiangle.js
+++ js/traiangle.js
@@ -1,6 +1,20 @@
-function calculateTriangleAre(){
-    //get triangle base
+function calculateTriangleArea(){
+    // //get triangle base
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = triangleBaseInput.value;
     const base = parseFloat(triangleBaseText);
+
+
+    // //get triangle height
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText); 
+
+    // calculate triangle area
+    const area = 0.5 * base * height; 
+    // console.log(area);
+
+    //display triangle are 
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area; 
 }
