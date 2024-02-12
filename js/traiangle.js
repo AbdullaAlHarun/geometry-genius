acbfb146c40281247913256a21ff93ef47e6bbf0
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-12 23:48:13 +0100

    added triangle base js!

---
+++ js/traiangle.js
@@ -0,0 +1,6 @@
+function calculateTriangleAre(){
+    //get triangle base
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+}
